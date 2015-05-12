c05683bf2043ce18e56a504a2e38fd8a1b1866fa
Rado Brglez <rado.brglez@gmail.com>
2015-05-12 10:33:41 +0200

    deluje test api Alternacija razen get list po funkciji

--- module/Produkcija/src/Produkcija/Repository/Alternacije.php
+++ module/Produkcija/src/Produkcija/Repository/Alternacije.php
@@ -5,6 +5,13 @@
  */
 namespace Produkcija\Repository;
 
+use Doctrine\Common\Collections\Criteria;
+use Doctrine\ORM\Tools\Pagination\Paginator;
+use DoctrineModule\Paginator\Adapter\Selectable;
+use DoctrineORMModule\Paginator\Adapter\DoctrinePaginator;
+use Max\Repository\AbstractMaxRepository;
+
+
 /**
  * Description of Alternacije
  *
@@ -15,7 +22,52 @@ class Alternacije
 {
     protected $sortOptions = [
         "default" => [
-            "id" => ["alias" => "id"]
+            "id" => ["alias" => "p.id"]
+        ],
+        "vse" => [
+            "id" => ["alias" => "p.id"]
+            
+            
         ]
     ];  
+    
+     public function getPaginator(array $options, $name = "default")
+    {
+        switch ($name) {
+            case "vse":
+                $qb   = $this->getVseQb($options);
+                $this->getSort($name, $qb);
+                return new DoctrinePaginator(new Paginator($qb));
+            case "default":
+                $this->expect(!empty($options['funkcija']), "Funkcija je obvezna", 770081);
+                $crit = new Criteria();
+                $e    = $crit->expr();
+
+                if (!empty($options[''])) {
+                    $oseba = $this->getEntityManager()->find('Produkcija\Entity\Funkcija', $options['funkcija']);
+                    $exp   = $e->eq('funkcija', $oseba);
+                }
+                $crit->andWhere($exp);
+                return new Selectable($this, $crit);
+        }
+    }
+    
+    
+      public function getVseQb($options)
+    {
+        $qb = $this->createQueryBuilder('p');
+        $e  = $qb->expr();
+        if (!empty($options['q'])) {
+
+            $naz = $e->like('p.id', ':id');
+
+            $qb->andWhere($e->orX($naz));
+
+            $qb->setParameter('id', "{$options['q']}%", "string");
+        }
+
+        return $qb;
+    }
+
+    
 }
