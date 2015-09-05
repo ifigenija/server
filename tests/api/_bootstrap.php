c73e03ba22ff997ab2e214cb60fde07b78c21bb0
Boris Lasic <boris@max.si>
2015-09-05 15:01:49 +0200

    test load-a baze zaradi spremembe v delovanju db modula

--- tests/api/_bootstrap.php
+++ tests/api/_bootstrap.php
@@ -1,4 +1,17 @@
 <?php
+
 // Here you can initialize variables that will be available to your tests
 
-const UUID_RE = '/^[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}$/';
+        const UUID_RE = '/^[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}$/';
+
+$db = $e->getSuite()->getModules()['Db'];
+
+$cu = new ReflectionMethod($db, 'connect');
+$cu->setAccessible(true);
+$cu->invoke($db);
+$cu = new ReflectionMethod($db, 'cleanup');
+$cu->setAccessible(true);
+$cu->invoke($db);
+$po = new ReflectionMethod($db, 'loadDump');
+$po->setAccessible(true);
+$po->invoke($db);
