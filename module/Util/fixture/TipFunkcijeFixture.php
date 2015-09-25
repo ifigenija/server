aba33bb8d28b26c0a39cde5631ac9d42b596a350
Rado Brglez <rado.brglez@gmail.com>
2015-09-25 12:03:02 +0200

    dodano polje tipFunkcije.sort

--- module/Util/fixture/TipFunkcijeFixture.php
+++ module/Util/fixture/TipFunkcijeFixture.php
@@ -48,31 +48,32 @@ class TipFunkcijeFixture
         if ($v[4]) {
             $o->setPodrocje($v[4]);
         }
+        $o->setSort($v[5]);
     }
 
     public function getData()
     {
         return [
             // tipi funkcije iz SLOGI:
-            ['01', 'Avtor', 'Avtorji', 'Avtorka', 'umetnik',],
-            ['02', 'Priredba', 'Priredba', 'Priredba', 'umetnik',],
-            ['03', 'Prevod', 'Prevod', 'Prevod', 'umetnik',],
-            ['04', 'Režija', 'Režija', 'Režija', 'umetnik',],
-            ['05', 'Dramaturgija', 'Dramaturgija', 'Dramaturgija', 'umetnik',],
-            ['06', 'Scenografija', 'Scenografija', 'Scenografija', 'umetnik',],
-            ['07', 'Kostumografija', 'Kostumografija', 'Kostumografija', 'umetnik',],
-            ['08', 'Koreografija', 'Koreografija', 'Koreografija', 'umetnik',],
-            ['09', 'Oblikovanje svetlobe in zvoka', 'Oblikovanje svetlobe in zvoka', 'Oblikovanje svetlobe in zvoka', 'umetnik',],
-            ['10', 'Oblikovanje maske', 'Oblikovanje maske', 'Oblikovanje maske', 'umetnik',],
-            ['11', 'Lektorstvo', 'Lektorstvo', 'Lektorstvo', 'umetnik',],
-            ['12', 'Glasbeni soustvarjalec', 'Glasbeni soustvarjalci', 'Glasbena soustvarjalka', 'umetnik',],
-            ['13', 'Intermedijski ustvarjalec', 'Intermedijski ustvarjalci', 'Intermedijski ustvarjalka', 'umetnik',],
-            ['14', 'Posebni efekti -izvedba in svetovanje', 'Posebni efekti -izvedba in svetovanje', 'Posebni efekti -izvedba in svetovanje', 'umetnik',],
-            ['15', 'Vloga', 'Intermedijski ustvarjalci', 'Intermedijska ustvarjalka', 'igralec',],
-            ['16', 'Produkcija, mentorstvo', 'Produkcija, mentorstvo', 'Produkcija, mentorstvo', 'umetnik',],
-            ['17', 'Tehnična podpora', 'Tehnična podpora', 'Tehnična podpora', 'tehnik',],
+            ['01', 'Avtor', 'Avtorji', 'Avtorka', 'umetnik',150,],
+            ['02', 'Priredba', 'Priredba', 'Priredba', 'umetnik',160,],
+            ['03', 'Prevod', 'Prevod', 'Prevod', 'umetnik',20,],
+            ['04', 'Režija', 'Režija', 'Režija', 'umetnik',30,],
+            ['05', 'Dramaturgija', 'Dramaturgija', 'Dramaturgija', 'umetnik',40,],
+            ['06', 'Scenografija', 'Scenografija', 'Scenografija', 'umetnik',60,],
+            ['07', 'Kostumografija', 'Kostumografija', 'Kostumografija', 'umetnik',70,],
+            ['08', 'Koreografija', 'Koreografija', 'Koreografija', 'umetnik',90,],
+            ['09', 'Oblikovanje svetlobe in zvoka', 'Oblikovanje svetlobe in zvoka', 'Oblikovanje svetlobe in zvoka', 'umetnik',100,],
+            ['10', 'Oblikovanje maske', 'Oblikovanje maske', 'Oblikovanje maske', 'umetnik',110,],
+            ['11', 'Lektorstvo', 'Lektorstvo', 'Lektorstvo', 'umetnik',50,],
+            ['12', 'Glasbeni soustvarjalec', 'Glasbeni soustvarjalci', 'Glasbena soustvarjalka', 'umetnik',80,],
+            ['13', 'Intermedijski ustvarjalec', 'Intermedijski ustvarjalci', 'Intermedijski ustvarjalka', 'umetnik',130,],
+            ['14', 'Posebni efekti -izvedba in svetovanje', 'Posebni efekti -izvedba in svetovanje', 'Posebni efekti -izvedba in svetovanje', 'umetnik',120,],
+            ['15', 'Vloga', 'Intermedijski ustvarjalci', 'Intermedijska ustvarjalka', 'igralec',10,],
+            ['16', 'Produkcija, mentorstvo', 'Produkcija, mentorstvo', 'Produkcija, mentorstvo', 'umetnik',140,],
+            ['17', 'Tehnična podpora', 'Tehnična podpora', 'Tehnična podpora', 'tehnik',180,],
             // specifika Ifigenije:
-            ['18', 'Inšpicient', 'Inšpicienti', 'Inšpicientka', 'inspicient',],
+            ['18', 'Inšpicient', 'Inšpicienti', 'Inšpicientka', 'inspicient',170,],
         ];
     }
 
