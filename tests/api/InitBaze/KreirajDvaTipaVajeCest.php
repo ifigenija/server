a0b23257b07c2a261c11a0a90e2458f70f536763
Rado Brglez <rado.brglez@gmail.com>
2015-10-28 14:41:49 +0100

    v delu #1431 -čiščenje api testov

---
+++ tests/api/InitBaze/KreirajDvaTipaVajeCest.php
@@ -0,0 +1,73 @@
+<?php
+
+/*
+ *  Licenca GPLv3
+ */
+
+namespace InitBaze;
+
+use ApiTester;
+
+/**
+ * Description of InitBazeVajaCest
+ * 
+ * reiramo 2 zapisain preverio število vseh zapisov 
+ *
+ * @author rado
+ */
+class KreirajDvaTipaVajeCest
+{
+
+    private $restUrl = '/rest/tipvaje';
+    private $obj1;
+    private $obj2;
+
+    public function _before(ApiTester $I)
+    {
+        $I->amHttpAuthenticated(\IfiTest\AuthPage::$admin, \IfiTest\AuthPage::$adminPass);
+    }
+
+    public function _after(ApiTester $I)
+    {
+        
+    }
+
+    /**
+     *  kreiramo zapis
+     * 
+     * @param ApiTester $I
+     */
+    public function create(ApiTester $I)
+    {
+        $data       = [
+            'sifra' => '9000',
+            'ime'   => 'zz',
+            'opis'  => 'znd',
+        ];
+        $this->obj1 = $ent        = $I->successfullyCreate($this->restUrl, $data);
+        $I->assertGuid($ent['id']);
+
+        // kreiramo še en zapis
+        $data       = [
+            'sifra' => '02',
+            'ime'   => 'aa',
+            'opis'  => 'and',
+        ];
+        $this->obj2 = $ent        = $I->successfullyCreate($this->restUrl, $data);
+        $I->assertGuid($ent['id']);
+    }
+
+    /**
+     * @depends create
+     * @param ApiTester $I
+     */
+    public function getListDefault(ApiTester $I)
+    {
+        $resp   = $I->successfullyGetList($this->restUrl, []);
+        $list   = $resp['data'];
+        codecept_debug($list);
+        $totRec = $resp['state']['totalRecords'];
+        $I->assertEquals(16, $resp['state']['totalRecords']);
+    }
+
+}
---
+++ tests/api/InitBaze/PonovnoKreirajDvaTipaVajeCest.php
@@ -0,0 +1,88 @@
+<?php
+
+/*
+ *  Licenca GPLv3
+ */
+
+namespace InitBaze;
+
+use ApiTester;
+use Codeception\Module;
+
+/**
+ * Description of PonovnoKreirajDveVajiCest
+ * 
+ * predvidevamo, da se je pred tem izvedel test KreirajDveVajiCest
+ * 
+ * ponovno kreiramo 2 zapisa  preverio število vseh zapisov  
+ *   - pričakujemo enako število, če smo uspeli pred tem inicializirati bazo
+ *   
+ * @author rado
+ */
+class PonovnoKreirajDvaTipaVajeCest
+{
+
+    private $restUrl = '/rest/tipvaje';
+    private $obj1;
+    private $obj2;
+
+    public function _before(ApiTester $I)
+    {
+        $I->amHttpAuthenticated(\IfiTest\AuthPage::$admin, \IfiTest\AuthPage::$adminPass);
+    }
+
+    public function _after(ApiTester $I)
+    {
+        
+    }
+
+    /**
+     * inicializira bazo  glede na DumpFunctional.sql
+     * 
+     * 
+     * @param ApiTester $I
+     */
+    public function initBaze(ApiTester $I)
+    {
+        $I->initDB();
+    }
+
+    /**
+     *  kreiramo zapis
+     * 
+     * @param ApiTester $I
+     */
+    public function create(ApiTester $I)
+    {
+        $data       = [
+            'sifra' => '9000',
+            'ime'   => 'zz',
+            'opis'  => 'znd',
+        ];
+        $this->obj1 = $ent        = $I->successfullyCreate($this->restUrl, $data);
+        $I->assertGuid($ent['id']);
+
+        // kreiramo še en zapis
+        $data       = [
+            'sifra' => '02',
+            'ime'   => 'aa',
+            'opis'  => 'and',
+        ];
+        $this->obj2 = $ent        = $I->successfullyCreate($this->restUrl, $data);
+        $I->assertGuid($ent['id']);
+    }
+
+    /**
+     * @depends create
+     * @param ApiTester $I
+     */
+    public function getListDefault(ApiTester $I)
+    {
+        $resp   = $I->successfullyGetList($this->restUrl, []);
+        $list   = $resp['data'];
+        codecept_debug($list);
+        $totRec = $resp['state']['totalRecords'];
+        $I->assertEquals(16, $resp['state']['totalRecords']);
+    }
+
+}
